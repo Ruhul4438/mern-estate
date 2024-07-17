761abed7e58603a885593606e35fea1998e2dc41
Ruhulamin <ruhulamin4427@gmail.com>
2024-07-17 10:47:44 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -260,7 +260,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
 
@@ -276,7 +278,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Discounted price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>)}
             
